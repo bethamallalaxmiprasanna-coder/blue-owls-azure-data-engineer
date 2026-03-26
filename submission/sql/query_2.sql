96dbcef39b19b91bcc1328722c59b04a67a7d810
bethamallalaxmiprasanna-coder <bethamalla.laxmi.prasanna@gmail.com>
2026-03-26 13:31:25 +0530

    Update query_2.sql

--- submission/sql/query_2.sql
+++ submission/sql/query_2.sql
@@ -1,7 +1,8 @@
-SELECT 
+SELECT
     seller_id,
-    COUNT(order_id) AS total_orders,
-    SUM(payment_value) AS total_revenue
+    COUNT(DISTINCT order_id) AS total_orders,
+    SUM(payment_value) AS total_revenue,
+    AVG(payment_value) AS avg_order_value
 FROM fact_order_items
 GROUP BY seller_id
-ORDER BY total_revenue DESC;
+ORDER BY total_revenue DESC; 
