14c8b447d5948e6850dddeb9a5c88d74bcc23911
bethamallalaxmiprasanna-coder <bethamalla.laxmi.prasanna@gmail.com>
2026-03-26 13:30:51 +0530

    Update query_1.sql

--- submission/sql/query_1.sql
+++ submission/sql/query_1.sql
@@ -1,6 +1,6 @@
-SELECT 
+SELECT
     DATE(order_purchase_timestamp) AS order_date,
     SUM(payment_value) AS total_revenue
 FROM fact_order_items
-GROUP BY order_date
+GROUP BY DATE(order_purchase_timestamp)
 ORDER BY order_date;
