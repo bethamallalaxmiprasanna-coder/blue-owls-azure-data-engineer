492bcc74cf4e86c40d3cb6f1dbfe28861be3f71a
bethamallalaxmiprasanna-coder <bethamalla.laxmi.prasanna@gmail.com>
2026-03-26 13:46:12 +0530

    Delete submission directory

--- submission/sql/query_1.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-    DATE(order_purchase_timestamp) AS order_date,
-    SUM(payment_value) AS total_revenue
-FROM fact_order_items
-GROUP BY DATE(order_purchase_timestamp)
-ORDER BY order_date;
--- submission/sql/query_2.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    seller_id,
-    COUNT(DISTINCT order_id) AS total_orders,
-    SUM(payment_value) AS total_revenue,
-    AVG(payment_value) AS avg_order_value
-FROM fact_order_items
-GROUP BY seller_id
-ORDER BY total_revenue DESC; 
